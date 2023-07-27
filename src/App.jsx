29800542eca2746ec6d8b95d6b35f57bcb620609
mrsahadatarefin <sahadatarifin806@gmail.com>
2023-07-27 21:48:53 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import "./App.css";
+import {fetchDataFromApi} from'./utils/api'
+import { useDispatch, useSelector } from "react-redux";
+import { getApiConfiguration } from "./store/homeSlice";
+  
+function App() {
+  
+   const dispatch = useDispatch()
+ const {url}=useSelector((state)=>state.home)
+  useEffect(()=>{
+    apiTesting()
+
+  },[])
+  const apiTesting = ()=>{
+    fetchDataFromApi('/movie/popular')
+    .then((res) =>{
+      console.log(res);
+    dispatch(getApiConfiguration(res))
+    } 
+    
+    )
+    
+  } 
+  return <div className="app">
+    
+    {url?.total_pages}
+  </div>;
+}
+
+export default App;
