b1e18726820b5ea3cbc7b4c00212b18c21fe3f06
mrsahadatarefin <sahadatarifin806@gmail.com>
2023-07-29 12:42:56 +0600

    header done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,9 @@ const Home = () => {
     return (
         <div className='homePage'>
             <HeroBanner/>
+            <div style={{height:1000}}>
+
+            </div>
         </div>
     );
 };
