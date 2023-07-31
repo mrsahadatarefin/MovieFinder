7abcc6e4226872f7ccbc42d8a2a7aa300c3e11c2
mrsahadatarefin <sahadatarifin806@gmail.com>
2023-07-31 21:44:44 +0600

    new added

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React from "react";
+
+import "./style.scss";
+import ContentWrapper from "../../components/contentWraper/ContentWeaper";
+
+
 
 const PageNotFound = () => {
     return (
-        <div>
-            
+        <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
         </div>
     );
 };
